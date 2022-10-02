ebc6ac31a29476146051d3dab057e406ebbc29da
Les Aker <me@lesaker.org>
2022-10-02 16:49:42 -0400

    update with stages

--- main.tf
+++ main.tf
@@ -32,6 +32,45 @@ resource "aws_api_gateway_rest_api" "this" {
   name = var.function_name
 }
 
+resource "aws_api_gateway_deployment" "this" {
+  rest_api_id = aws_api_gateway_rest_api.this.id
+  variables   = var.stage_variables
+}
+
+resource "aws_api_gateway_stage" "this" {
+  deployment_id = aws_api_gateway_deployment.this.id
+  rest_api_id   = aws_api_gateway_rest_api.this.id
+  stage_name    = "prod"
+}
+
+resource "aws_api_gateway_method_settings" "settings" {
+  rest_api_id = aws_api_gateway_rest_api.this.id
+  stage_name  = aws_api_gateway_stage.this.stage_name
+  method_path = "*/*"
+
+  settings {
+    metrics_enabled = true
+    logging_level   = "INFO"
+  }
+}
+
+resource "aws_api_gateway_domain_name" "this" {
+  domain_name     = var.hostname
+  certificate_arn = module.certificate.arn
+}
+
+resource "aws_api_gateway_base_path_mapping" "this" {
+  api_id      = aws_api_gateway_rest_api.this.id
+  stage_name  = aws_api_gateway_stage.this.stage_name
+  domain_name = aws_api_gateway_domain_name.this.domain_name
+}
+
+resource "aws_api_gateway_resource" "this" {
+  rest_api_id = aws_api_gateway_rest_api.this.id
+  parent_id   = aws_api_gateway_rest_api.this.root_resource_id
+  path_part   = "{path+}"
+}
+
 resource "aws_api_gateway_method" "this" {
   rest_api_id   = aws_api_gateway_rest_api.this.id
   resource_id   = aws_api_gateway_resource.this.id
@@ -44,17 +83,6 @@ resource "aws_api_gateway_method" "root" {
   resource_id   = aws_api_gateway_rest_api.this.root_resource_id
   http_method   = "ANY"
   authorization = "NONE"
-}
-
-resource "aws_api_gateway_method_settings" "settings" {
-  rest_api_id = aws_api_gateway_rest_api.this.id
-  stage_name  = aws_api_gateway_deployment.this.stage_name
-  method_path = "*/*"
-
-  settings {
-    metrics_enabled = true
-    logging_level   = "INFO"
-  }
 }
 
 resource "aws_api_gateway_integration" "this" {
@@ -73,36 +101,5 @@ resource "aws_api_gateway_integration" "root" {
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = module.lambda.invoke_arn
-}
-
-resource "aws_api_gateway_deployment" "this" {
-  depends_on = [
-    aws_api_gateway_method.this,
-    aws_api_gateway_method.root,
-    aws_api_gateway_integration.this,
-    aws_api_gateway_integration.root,
-    aws_api_gateway_resource.this,
-  ]
-
-  rest_api_id = aws_api_gateway_rest_api.this.id
-  stage_name  = "prod"
-  variables   = var.stage_variables
-}
-
-resource "aws_api_gateway_resource" "this" {
-  rest_api_id = aws_api_gateway_rest_api.this.id
-  parent_id   = aws_api_gateway_rest_api.this.root_resource_id
-  path_part   = "{path+}"
-}
-
-resource "aws_api_gateway_domain_name" "this" {
-  domain_name     = var.hostname
-  certificate_arn = module.certificate.arn
-}
-
-resource "aws_api_gateway_base_path_mapping" "this" {
-  api_id      = aws_api_gateway_rest_api.this.id
-  stage_name  = aws_api_gateway_deployment.this.stage_name
-  domain_name = aws_api_gateway_domain_name.this.domain_name
 }
 
