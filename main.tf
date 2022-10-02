c71a2a98ff3773563573943dcef32a292c748fea
Les Aker <me@lesaker.org>
2022-10-02 16:53:01 -0400

    remove extra resource

--- main.tf
+++ main.tf
@@ -78,13 +78,6 @@ resource "aws_api_gateway_method" "this" {
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_method" "root" {
-  rest_api_id   = aws_api_gateway_rest_api.this.id
-  resource_id   = aws_api_gateway_rest_api.this.root_resource_id
-  http_method   = "ANY"
-  authorization = "NONE"
-}
-
 resource "aws_api_gateway_integration" "this" {
   rest_api_id             = aws_api_gateway_rest_api.this.id
   resource_id             = aws_api_gateway_resource.this.id
