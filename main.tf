16c6fcdadec70dedd3ba5601cd1c77c24cb27fd7
Les Aker <me@lesaker.org>
2022-10-03 11:53:43 -0400

    update stage vars to correct location

--- main.tf
+++ main.tf
@@ -34,13 +34,13 @@ resource "aws_api_gateway_rest_api" "this" {
 
 resource "aws_api_gateway_deployment" "this" {
   rest_api_id = aws_api_gateway_rest_api.this.id
-  variables   = var.stage_variables
 }
 
 resource "aws_api_gateway_stage" "this" {
   deployment_id = aws_api_gateway_deployment.this.id
   rest_api_id   = aws_api_gateway_rest_api.this.id
   stage_name    = "prod"
+  variables     = var.stage_variables
 }
 
 resource "aws_api_gateway_method_settings" "settings" {
